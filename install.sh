2318ab82af184747784480cb28055d8875292b37
eliza <sech1p@disroot.org>
2025-04-28 17:17:01 +0200

    ➕ Add support to macOS in configuration installer
    Installation VS Code on macOS by default way (into Applications), doesn't add code binary to PATH. Due to different structure of this system compared to Linux - you can handle macOS apps by `open` command.

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cat extensions.txt | xargs -L 1 echo code --install-extension
+user_os=$(uname)
+
+if ! [[ $user_os == "Darwin" ]]; then
+    # Non macOS systems (Linux, BSD's, etc.)
+    cat extensions.txt | xargs -L 1 echo code --install-extension
+else
+    # macOS systems
+    cat extensions.txt | xargs -L 1 echo open -a Visual\ Studio\ Code --install-extension
+fi
